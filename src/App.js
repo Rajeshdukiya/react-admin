15bb42963a7a0ccbe92a23910a4bffd2a5b7abc9
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-06-19 12:46:20 +0530

    Topbar-setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+// import { Route, Routes } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { Dashboard } from "@mui/icons-material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +12,10 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+          <Topbar/>
+         
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
